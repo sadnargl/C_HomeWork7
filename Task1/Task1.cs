b047ff69cd09aec46283eeb36eb8bb315bb5b004
Aleksey.Alekseev <sadnargl@gmail.com>
2023-02-23 18:31:49 +0300

    задача 47

---
+++ Task1/Task1.cs
@@ -0,0 +1,22 @@
+﻿int x1 = InputFigureFromConsole ("Введите первое число: ");
+int x2 = InputFigureFromConsole ("Введите второе число: ");
+long res = x1;
+for (int j = 1; j <= x2-1; j++)
+{
+    res = res * x1;
+}
+Console.WriteLine ($"{x1} в степени {x2} равняется {res}");
+
+int InputFigureFromConsole (string invitation)
+{
+    Console.Write (invitation);
+    int i;
+    while (!int.TryParse (Console.ReadLine(), out i))
+    {
+        Console.WriteLine ("Ошибка ввода числа");
+        Console.Write (invitation);
+    }
+    return i;
+}
+
+
