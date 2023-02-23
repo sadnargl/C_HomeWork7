e52555a57e0f199405034e5decc0a57227c5fe3a
Aleksey.Alekseev <sadnargl@gmail.com>
2023-02-23 18:32:11 +0300

    задача 52

---
+++ Task3/Task3.cs
@@ -0,0 +1,45 @@
+﻿byte [,] result = RandomFeelTwoDimensionsArray (5,5);
+PrintArray (result);
+Console.WriteLine ();
+PrintMiddleZumInRow (result);
+
+Byte [,] RandomFeelTwoDimensionsArray (int FirstLength, int SecondLength)
+{
+    Byte [,] Array = new Byte [FirstLength,SecondLength];
+    Random rnd = new Random ();
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+       for (int j = 0; j < Array.GetLength(1); j++)
+       {
+        Array [i,j]= (byte)rnd.NextInt64 (0,255);
+       } 
+    }
+    return Array;
+}
+
+void PrintMiddleZumInRow (byte [,] Array)
+{
+    int columnzum;
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        columnzum = 0;
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            columnzum = columnzum + Array [j,i];
+        } 
+        Console.Write ($"{(double)columnzum/Array.GetLength(0)} ");
+    }
+}
+
+void PrintArray (byte [,] Array)
+{
+
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+          Console.Write ($"{Array [i,j]} ");  
+        }
+        Console.WriteLine ();
+    }
+}
